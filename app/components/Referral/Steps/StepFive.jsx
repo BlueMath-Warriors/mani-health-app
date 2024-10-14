00a3f7db2b5da852de529068acecb8b7a6db4168
NivyanB <bnivyan09@gmail.com>
2024-10-15 00:20:18 +0500

    add referral step 5

--- app/components/Referral/Steps/StepFive.jsx
+++ app/components/Referral/Steps/StepFive.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { useState } from "react";
+import Input from "../../common/Input";
 
-const StepFive = () => {
-  return <div>step 5</div>;
+const StepTwo = () => {
+  const [formData, setFormData] = useState({
+    insuranceCompany: "",
+    policyNumber: "",
+    claimNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <form className="grid grid-cols-12 gap-x-4 gap-y-3 w-full items-start">
+      <Input
+        label="Insurance Company"
+        name="insuranceCompany"
+        value={formData.insuranceCompany}
+        onChange={handleChange}
+        required
+        className="col-span-6"
+      />
+
+      <Input
+        label="Policy Number"
+        name="policyNumber"
+        value={formData.policyNumber}
+        onChange={handleChange}
+        required
+        className="col-span-6"
+      />
+
+      <Input
+        label="Claim Number"
+        name="claimNumber"
+        value={formData.claimNumber}
+        onChange={handleChange}
+        required
+        className="col-span-12"
+      />
+    </form>
+  );
 };
 
-export default StepFive;
+export default StepTwo;
