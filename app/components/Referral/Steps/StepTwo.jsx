0ea6cfa5cb21af26894b0f08e65788c34b518fda
NivyanB <bnivyan09@gmail.com>
2024-10-15 00:02:25 +0500

    add generic input component

--- app/components/Referral/Steps/StepTwo.jsx
+++ app/components/Referral/Steps/StepTwo.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useState } from "react";
+import Input from "../../common/Input";
 
 const StepTwo = () => {
-  return <div>step 2</div>;
+  const [formData, setFormData] = useState({
+    fullName: "",
+    address: "",
+    phone: "",
+    employerName: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <form className="grid grid-cols-12 gap-x-4 gap-y-3 w-full items-start">
+      <Input
+        label="First Name"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        required
+        className="col-span-6"
+      />
+
+      <Input
+        label="Address"
+        name="address"
+        value={formData.address}
+        onChange={handleChange}
+        className="col-span-6"
+      />
+
+      <Input
+        label="Phone Number"
+        name="phone"
+        value={formData.phone}
+        onChange={handleChange}
+        required
+        className="col-span-6"
+      />
+
+      <Input
+        label="Employer Name"
+        name="employerName"
+        value={formData.employerName}
+        onChange={handleChange}
+        className="col-span-6"
+      />
+    </form>
+  );
 };
 
 export default StepTwo;
