163151b585e6933a7e6767c915121c6e0e375617
Saim <muhammadsaimali@gmail.com>
2024-10-14 18:33:15 +0500

    Complete Footer

---
+++ Constants/navbarDropdownLinks.js
@@ -0,0 +1,51 @@
+export const servicesLinks = [
+    { name: "Comprehensive Orthopaedic Care", destination: "/" },
+    { name: "Enhanced Recovery After Surgery (ERAS)", destination: "/" },
+    { name: "Durable Medical Equipment (DME)", destination: "/" },
+    { name: "Revitalize and Repair Solutions", destination: "/" },
+    { name: "Maximum Medical Improvement (MMI)", destination: "/" },
+    { name: "Hip and Knee Orthopaedic Specialist", destination: "/" },
+    { name: "Spinal Orthopaedic Surgeon", destination: "/" },
+    { name: "Foot and Ankle Orthopaedic Specialist", destination: "/" },
+    { name: "Hand and Upper Extremity Orthopaedic Surgeon", destination: "/" },
+    { name: "Kyphoplasty Services", destination: "/" },
+  ];
+  
+  export const legalSupportLinks = [
+    {
+      name: "Personal Injury Evaluation",
+      destination: "/",
+    },
+    {
+      name: "Expert Medical Advisors",
+      destination: "/",
+    },
+    {
+      name: "Independent Medical Evaluation (IME)",
+      destination: "/",
+    },
+    {
+      name: "Expert Witness Reports",
+      destination: "/",
+    },
+    {
+      name: "Emergency Medical Condition (EMC) Evaluation",
+      destination: "/",
+    },
+  ];
+  
+  export const workersCompLinks = [
+    { name: "Adjusters", destination: "/" },
+    { name: "Lawyers and Attorneys", destination: "/" },
+    { name: "Payers", destination: "/" },
+  ];
+
+  export const quickLinks = [
+    { name: "Home", destination: "/"},
+    { name: "About Us", destination: "/"},
+    { name: "Services", destination: "/"},
+    { name: "Legal Support", destination: "/"},
+    { name: "Workers Comp", destination: "/"},
+    { name: "Senior Living", destination: "/"},
+    { name: "Contact", destination: "/"},
+  ];
