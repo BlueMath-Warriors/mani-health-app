9be10da4a573751a50680c838e760d8c6a2380d6
Saim <muhammadsaimali@gmail.com>
2024-10-23 22:05:30 +0500

    Create initial design

---
+++ Constants/workersCompData.js
@@ -0,0 +1,17 @@
+export const WORKERS_TYPES = [
+  {
+    title: "Adjusters",
+    text: "Efficient claims management and compliance-focused solutions.",
+    imageSrc: "/images/adjusters_image.svg",
+  },
+  {
+    title: "Lawyers and Attorneys",
+    text: "Accurate medical reporting and expertise for legal professionals.",
+    imageSrc: "/images/lawyers_attorney_image.svg",
+  },
+  {
+    title: "Payers",
+    text: "Cost-effective, compliant care partnerships.",
+    imageSrc: "/images/payer_image.svg",
+  },
+];
---
+++ app/workers-comp/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WorkersCompIntroSection from '../components/workers-comp/WorkersCompIntroSection';
+
+const WorkersComp = () => {
+  return (
+    <>
+        <WorkersCompIntroSection />
+    </>
+  )
+}
+
+export default WorkersComp;
