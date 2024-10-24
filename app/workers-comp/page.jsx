9718677df4937b7d619a67704e99dd0ec936052e
Saim <muhammadsaimali@gmail.com>
2024-10-24 23:00:06 +0500

    Add Referal action Page

--- app/workers-comp/page.jsx
+++ app/workers-comp/page.jsx
@@ -3,6 +3,7 @@ import WorkersCompIntroSection from '../components/workers-comp/WorkersCompIntro
 import WorkersCompListSection from '../components/workers-comp/WorkersCompListSection';
 import { WORKERS_COMPENSATION_SERVICES } from '@/Constants/workersCompData';
 import WorkersCompDetailsSection from '../components/workers-comp/WorkersCompDetailsSection';
+import ReferralAction from '../components/common/ReferralAction';
 
 const WorkersComp = () => {
   return (
@@ -10,6 +11,7 @@ const WorkersComp = () => {
         <WorkersCompIntroSection />
         <WorkersCompDetailsSection />
         <WorkersCompListSection content={WORKERS_COMPENSATION_SERVICES} />
+        <ReferralAction />
     </>
   )
 }
