4a012672136b7ebbdd37d4bc051f780910d50090
Saim <muhammadsaimali@gmail.com>
2024-10-29 16:56:19 +0500

    Merge with base branch

--- app/components/Home/index.jsx
+++ app/components/Home/index.jsx
@@ -7,28 +7,6 @@ import AboutAMHI from "./AboutAMHI";
 import ReferralAction from "../common/ReferralAction";
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <>
-      <div class="preloader" style={{ display: "none" }}>
-        <div class="centrize full-width">
-          <div class="vertical-center">
-            <div class="spinner">
-              <div class="double-bounce1"></div>
-              <div class="double-bounce2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex w-full flex-col items-center justify-center">
-        <SlickCarousel />
-        <WhoWeServe />
-        <AboutAMHI />
-        <WhatWeOffer />
-        <ComprehensiveCare />
-        <ReferralAction />
-      </div>
-    </>
-=======
     <div className="flex w-full flex-col items-center justify-center">
       <SlickCarousel />
       <WhoWeServe />
@@ -37,7 +15,6 @@ const Home = () => {
       <ComprehensiveCare />
       <ReferralAction />
     </div>
->>>>>>> a23399218e6e2a8a8b61ed9f75613dd64c6760a5
   );
 };
 
