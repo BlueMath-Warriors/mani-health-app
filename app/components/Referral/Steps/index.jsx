2fb68896aeb94573483f57ba67ffdf5e31348afc
Saim <muhammadsaimali@gmail.com>
2024-10-31 20:21:17 +0500

    Add Referal toast

--- app/components/Referral/Steps/index.jsx
+++ app/components/Referral/Steps/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import Image from "next/image";
 import StepsTracker from "../Steps-Tracker";
 import StepOne from "./StepOne";
@@ -17,7 +18,7 @@ import {
 } from "@/app/constants/steps";
 import { submitReferralForm } from "@/app/lib/api";
 
-const Steps = ({ currentStep, nextStep, prevStep }) => {
+const Steps = ({ currentStep, nextStep, prevStep, showToast }) => {
   const [position, setPosition] = useState();
   const [showError, setShowError] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -54,6 +55,13 @@ const Steps = ({ currentStep, nextStep, prevStep }) => {
 
     try {
       const response = await submitReferralForm(formData);
+      showToast();
+      setPosition();
+      setUploadedFiles([]);
+      setPatientInfo(INITIAL_PATIENT_INFO);
+      setAccidentDetails(INITIAL_ACCIDENT_DETAILS);
+      setInsuranceInfo(INITIAL_INSURANCE_INFO);
+      setAdjusterInfo(INITIAL_ADJUSTER_INFO);
       console.log("Form submitted successfully:", response);
     } catch (error) {
       console.error("Error submitting form:", error);
@@ -145,52 +153,54 @@ const Steps = ({ currentStep, nextStep, prevStep }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-11/12 lg:max-w-[824px] lg:w-full px-6 py-5 lg:p-16 bg-white rounded-lg referral-card-shadow transition-all duration-200 ease-linear">
-      <div className="flex w-full items-center justify-center relative">
-        <Image
-          src={"/images/leftTop.svg"}
-          alt="left top image"
-          width={88}
-          height={88}
-          className="hidden xl:block absolute -top-20 -left-56 "
-        />
-        <Image
-          src={"/images/leftMiddle.svg"}
-          alt="left middle image"
-          width={40}
-          height={60}
-          className="hidden xl:block absolute top-60 -left-56 "
-        />
-        <Image
-          src={"/images/shapeRight.svg"}
-          alt="right image"
-          width={40}
-          height={60}
-          className="hidden xl:block absolute top-60 -right-56 "
-        />
+    <>
+      <div className="flex flex-col items-center justify-center w-11/12 lg:max-w-[824px] lg:w-full px-6 py-5 lg:p-16 bg-white rounded-lg referral-card-shadow transition-all duration-200 ease-linear">
+        <div className="flex w-full items-center justify-center relative">
+          <Image
+            src={"/images/leftTop.svg"}
+            alt="left top image"
+            width={88}
+            height={88}
+            className="hidden xl:block absolute -top-20 -left-56 "
+          />
+          <Image
+            src={"/images/leftMiddle.svg"}
+            alt="left middle image"
+            width={40}
+            height={60}
+            className="hidden xl:block absolute top-60 -left-56 "
+          />
+          <Image
+            src={"/images/shapeRight.svg"}
+            alt="right image"
+            width={40}
+            height={60}
+            className="hidden xl:block absolute top-60 -right-56 "
+          />
+        </div>
+        <div className="w-full flex flex-col items-center justify-center ">
+          <h3 className="text-dark text-lg mb-0 sm:mb-4 md:mb-8 font-semibold">
+            {STEPS_TITLES[currentStep - 1]}
+          </h3>
+          <StepsTracker currentStep={currentStep} />
+          {steps[currentStep - 1]}
+        </div>
+        <div className="flex w-full flex-col gap-4 my-4 md:my-12">
+          {currentStep === 6 ? (
+            <Button onClick={handleReferralSubmission}>
+              {loading ? "...submitting" : "Submit"}
+            </Button>
+          ) : (
+            <Button onClick={handleNextStep}>Next</Button>
+          )}
+          {currentStep > 1 && (
+            <Button onClick={prevStep} variant="primary-outline">
+              Previous
+            </Button>
+          )}
+        </div>
       </div>
-      <div className="w-full flex flex-col items-center justify-center ">
-        <h3 className="text-dark text-lg mb-0 sm:mb-4 md:mb-8 font-semibold">
-          {STEPS_TITLES[currentStep - 1]}
-        </h3>
-        <StepsTracker currentStep={currentStep} />
-        {steps[currentStep - 1]}
-      </div>
-      <div className="flex w-full flex-col gap-4 my-4 md:my-12">
-        {currentStep === 6 ? (
-          <Button onClick={handleReferralSubmission}>
-            {loading ? "...submitting" : "Submit"}
-          </Button>
-        ) : (
-          <Button onClick={handleNextStep}>Next</Button>
-        )}
-        {currentStep > 1 && (
-          <Button onClick={prevStep} variant="primary-outline">
-            Previous
-          </Button>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
--- app/components/Referral/index.jsx
+++ app/components/Referral/index.jsx
@@ -1,26 +1,37 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
 import Steps from "./Steps";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Referral = () => {
   const [step, setStep] = useState(1);
 
-  return (
-    <div className="relative w-full flex flex-col items-center justify-center gap-4 font-medium">
-      <h2 className="text-base lg:text-[39px] text-dark mb-2 font-semibold">
-        Referral Form Request
-      </h2>
-      <p className="text-xs lg:text-base text-[#818181] mb-4">
-        Please follow the steps
-      </p>
+  const showToast = () => {
+    toast.success("Form submitted successfully", {
+      autoClose: 4000,
+    });
+  };
 
-      <Steps
-        currentStep={step}
-        nextStep={() => setStep(step + 1)}
-        prevStep={() => setStep(step - 1)}
-      />
-    </div>
+  return (
+    <>
+      <ToastContainer />
+      <div className="relative w-full flex flex-col items-center justify-center gap-4 font-medium">
+        <h2 className="text-base lg:text-[39px] text-dark mb-2 font-semibold">
+          Referral Form Request
+        </h2>
+        <p className="text-xs lg:text-base text-[#818181] mb-4">
+          Please follow the steps
+        </p>
+
+        <Steps
+          currentStep={step}
+          nextStep={() => setStep(step + 1)}
+          prevStep={() => setStep(step - 1)}
+          showToast={showToast}
+        />
+      </div>
+    </>
   );
 };
 
